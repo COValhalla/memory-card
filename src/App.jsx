d54441acfd6b88aeb1858ad7ff09efc32b7c848a
COValhalla <josephvellella@proton.me>
2022-08-12 10:37:28 -0600

    Added scoreboard.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Cards from './components/Cards';
 
 export default function App() {
+  const [score, setScore] = useState({ currentScore: 0, bestScore: 0 });
+
   return (
     <div className="App">
-      <Header />
+      <Header score={score} />
       <Cards />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Header() {
+export default function Header(props) {
+  console.log(props.score);
   return (
-    <div className="mx-auto  flex max-w-5xl bg-slate-500">
-      <h1 className="font-fantasy text-4xl">Final Fantasy: Memory Edition</h1>
+    <div className="mx-auto flex max-w-5xl  place-items-center justify-around bg-slate-500 p-8 font-fantasy text-4xl">
+      <h1 className="">Final Fantasy: Memory Edition</h1>
+      <div>
+        <h1>Current Score: {props.score.currentScore} </h1>
+        <h1>Best Score: {props.score.bestScore}</h1>
+      </div>
     </div>
   );
 }
