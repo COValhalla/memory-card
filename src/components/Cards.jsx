94b9b2966cb1855c527ddc2214018a4c05f42d78
COValhalla <josephvellella@proton.me>
2022-08-12 12:00:36 -0600

    Changed images. Increased to 12 total images.

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,15 +3,26 @@ import React from 'react';
 export default function Cards(props) {
   const randomImages = props.imageArray.map((img) => {
     return (
-      <div key={img.id} className="border-2 border-slate-400">
-        <img onClick={props.shuffleImages} src={img.src} alt="" id={img.id} />
+      <div
+        key={img.id}
+        className="flex w-32 justify-center rounded border border-slate-400 p-2 hover:border-slate-500"
+      >
+        <img
+          onClick={props.shuffleImages}
+          src={img.src}
+          alt=""
+          id={img.id}
+          className="h-36"
+        />
       </div>
     );
   });
 
   return (
-    <div className="mx-auto flex max-w-3xl flex-wrap justify-center gap-6 bg-slate-800 p-8">
-      {randomImages}
+    <div className=" bg-slate-800 p-8">
+      <div className="mx-auto flex  max-w-4xl flex-wrap justify-center gap-6 bg-slate-800 p-8">
+        {randomImages}
+      </div>
     </div>
   );
 }
--- src/components/images.jsx
+++ src/components/images.jsx
@@ -1,12 +1,16 @@
 const images = [
   { id: 1, src: 'src/assets/images/aerith.webp' },
-  { id: 2, src: 'src/assets/images/cait.webp' },
-  { id: 3, src: 'src/assets/images/cloud.webp' },
-  { id: 4, src: 'src/assets/images/jenova.webp' },
-  { id: 5, src: 'src/assets/images/lucrecia.webp' },
-  { id: 6, src: 'src/assets/images/sephiroth.webp' },
-  { id: 7, src: 'src/assets/images/tifa.webp' },
-  { id: 8, src: 'src/assets/images/zach.webp' },
+  { id: 2, src: 'src/assets/images/barret.webp' },
+  { id: 3, src: 'src/assets/images/cait.webp' },
+  { id: 4, src: 'src/assets/images/cid.webp' },
+  { id: 5, src: 'src/assets/images/cloud.webp' },
+  { id: 6, src: 'src/assets/images/hojo.webp' },
+  { id: 7, src: 'src/assets/images/jessie.webp' },
+  { id: 8, src: 'src/assets/images/red.webp' },
+  { id: 9, src: 'src/assets/images/sephiroth.webp' },
+  { id: 10, src: 'src/assets/images/tifa.webp' },
+  { id: 11, src: 'src/assets/images/vincent.webp' },
+  { id: 12, src: 'src/assets/images/yuffie.webp' },
 ];
 
 export default images;
